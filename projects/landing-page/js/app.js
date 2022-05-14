3678e16aa8bf2480f7bb6956f8c0d4aaee6f3a79
nickync <lizheng9024@gmail.com>
2022-05-14 01:14:58 -0400

    'work'

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -23,6 +23,9 @@
  * 
 */
 
+//get section nodes
+const section = document.querySelectorAll('section')
+
 
 /**
  * End Global Variables
@@ -30,33 +33,97 @@
  * 
 */
 
+// create navbar menu button
+function createButton() {
+    
+    return section.forEach(item => {
+        // create li element
+        const navList = document.createElement('li')
 
+        // get li's parent element ul
+        const navBar = document.getElementById('navbar__list')
 
+        // get menu button's names
+        const textNode = document.createTextNode(item.dataset.nav)
+    
+        // create anchor tag
+        const aLinks = document.createElement('a')
+
+        // set anchor tag's name
+        aLinks.appendChild(textNode)
+
+// add scrollTo events to anchor
+        //aLinks.href = `#${item.id}`
+        aLinks.addEventListener('click', ()=> {
+            document.querySelector(`#${item.id}`).scrollIntoView({
+                behavior:"smooth"
+            })
+        })
+
+        // append anchor tag to li
+        navList.appendChild(aLinks)
+
+        // append li to ul
+        navBar.appendChild(navList)
+    })
+}
+
+createButton()
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
+// Check if element is in viewport
 
+function isActive(elem) {
 
-// Add class 'active' to section when near top of viewport
+        // get corodinates of element
+        let position = elem.getBoundingClientRect()
 
+        // return true if in viewport
+        return (position.top + window.innerHeight >= 0 &&
+            position.left >= 0 &&
+            position.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            position.right <= (window.innerWidth || document.documentElement.clientWidth)
+        ) 
+}
 
-// Scroll to anchor ID using scrollTO event
+// filter section and add class
 
+function addClass() {
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+    return section.forEach(item => {
 
-// Build menu 
+        // add class when true and remove when false
+        if (isActive(item)) {
+            item.classList.add('your-active-class')
+        } else if (isActive(item) === false) {
+            item.classList.remove('your-active-class')
+        }
+    })
+}
 
-// Scroll to section on link click
+// Add active class when scrolling
+window.addEventListener('scroll', addClass)
 
-// Set sections as active
+// Display scroll to top button
+let button = document.getElementById('btn')
 
+// display button when scroll over section 1
+window.onscroll = () => {
+    if (document.body.scrollTop > document.getElementById('section1').clientHeight) {
+        button.style.display = 'block'
+    } else {
+        button.style.display = 'none'
+    }
+}
 
+// return to top
+function toTop() {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+}
