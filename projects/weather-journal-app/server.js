437c27d6d9b44e7521827fa61494ed76f2b06c84
nickync <lizheng9024@gmail.com>
2022-05-28 22:51:23 -0400

    work

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,38 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express')
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
+const bodyParser = require('body-parser')
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.get('/info', (req, res) => {
+    res.send(projectData)
+})
+
+app.post('/Adddata', (req, res) => {
+    projectData['temperature'] = req.body['temperature']
+    projectData['date'] = req.body['date']
+    projectData['user_response'] = req.body['user_response']
+    res.send(projectData)
+})
+
+app.listen(80, () => {
+    console.log('server is running at 127.0.0.1:80')
+})
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,39 @@
 /* Global Variables */
+// const zip = document.getElementById('zip')
+let zip = 11432
+let temp = ''
+const btn = document.getElementById('generate')
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = '38291b0a7dea99d63631951d24246b2e&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// get api link
+async function getLink() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`
+    return url
+}
+
+// fetch API data
+async function getData(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        temp = data
+        console.log(temp)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+// assgin function to button
+btn.addEventListener('click', generateEnv)
+
+async function generateEnv() {
+   const url = await getLink()
+   const temp = await getData(url)
+}
